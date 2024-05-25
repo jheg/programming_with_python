a3bf7a0266d35640bf0531ea8519179a161d0e52
Jason Hegarty <jason@jheg.co.uk>
2024-05-25 09:32:52 +0100

    Updadted to include info about comments and docstrings

--- Using Python/using_python.py
+++ Using Python/using_python.py
@@ -3,3 +3,18 @@ def double_list(input_list):
         input_list[index] = element
 
     return input_list
+
+# code comments
+
+# Documentation strings
+"""Comments should be complete sentences. 
+
+The first word should be capitalized, unless it is an 
+identifier that begins with a lower case letter (never alter the case of identifiers!).
+"""
+
+"""One liner docstrings, keep the closing tripple quotes on the same line."""
+
+# single line comment     
+# Use inline comments sparingly.
+name = 'Jason'      # An inline comment is a comment on the same line as a statement. Inline comments should be separated by at least two spaces from the statement. They should start with a # and a single space.
